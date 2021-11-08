c13edeaeb0b9e5bb20432bc8a0b8fc1cedac0c4a
Siva Kotaprolu <siva.kotaprolu@outlook.com>
2021-11-08 13:01:01 +0530

    Add project files.

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+   partial class Class1
+    {
+        public void m2()
+        {
+            int bb = 50;
+            //  string aa =Convert.ToString(bb);
+            int aa = 500;
+            int aaa = aa ^ bb;
+            Console.WriteLine(aaa);
+        }
+
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+
+   partial class Class1
+    {
+
+
+
+
+        public void Show(ref int val)
+        {
+            int a = 5;
+            val =a ;
+            val *= val;
+            
+           // Console.WriteLine("Value inside the show function " + val);
+
+        }
+
+        static void Main(string[] args)
+        {
+          //int vae = 50;
+              Class1 a1 = new Class1();
+            /*  Console.WriteLine("Value before calling the function " + vae);
+             a.Show(ref vae);
+             Console.WriteLine("Value after calling the function " + vae);*/
+            a1.m2();
+        }
+    }
+}
